694e64aeea2b38d364cd6662785fbb3485c62bbf
matheusgondra <matheusgondra951@gmail.com>
2022-08-31 13:21:28 -0300

    Criando método de login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,5 +1,6 @@
 const database = require("../models");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 class UserController {
     static async createUser(req, res) {
@@ -9,6 +10,27 @@ class UserController {
             const createdUser = await database.User.create(newUser);
             createdUser.password = undefined;
             return res.status(201).json(createdUser);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async loginUser(req, res) {
+        const { username, password } = req.body;
+        try {
+            const user = await database.User.findOne({ where: { username: username }});
+
+            if(!user) {
+                return res.status(404).json({ error: "Usuário não encontrado" });
+            }
+            if(!await bcrypt.compare(password, user.password)) {
+                return res.status(404).json({ error: "Usuário invalido"});
+            }
+
+            const token = jwt.sign({ id: user.id }, process.env.SECRET, { expiresIn: 86400 });
+
+            return res.status(200).json({ auth: true, token: token });
+
         } catch (error) {
             return res.status(500).json(error.message);
         }
