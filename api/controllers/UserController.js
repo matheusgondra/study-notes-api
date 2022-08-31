338985d84306b624164aa0debac9417d0be28b9c
matheusgondra <matheusgondra951@gmail.com>
2022-08-31 13:07:10 -0300

    Criptografando senha de novos usuários

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,9 +1,11 @@
 const database = require("../models");
+const bcrypt = require("bcrypt");
 
 class UserController {
     static async createUser(req, res) {
         const newUser = req.body;
         try {
+            newUser.password = await bcrypt.hash(newUser.password, 12);
             const createdUser = await database.User.create(newUser);
             createdUser.password = undefined;
             return res.status(201).json(createdUser);
